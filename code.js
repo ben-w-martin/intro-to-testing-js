5ea868ce67f46d32257de008acdc777a7c576c37
Benjamin Martin <ben.w.martin17@gmail.com>
2023-10-17 18:20:56 -0400

    refactored l. 21 to omit unnecessary condition

--- code.js
+++ code.js
@@ -18,15 +18,14 @@ const sayHello = (input) => {
     return "Hello, World!";
     } else if (Number(input) === parseFloat(input)) {
     return "Hello, World!";
-    } else if (isNaN(input) && isNaN(parseFloat(input)) === false){
+    } else if (isNaN(parseFloat(input)) === false){
     return "Hello, World!";
     } else {
     return `Hello, ${input}.`;
     }
 }
 
-console.log(isNaN("2a"));
-console.log(isNaN(parseFloat("2a")) === false);
-console.log(parseFloat("fasdfa"));
-
-// What is true of "2a" that is not true of any other string?
+// console.log(isNaN("2a"));
+// console.log(isNaN(parseFloat("2a")) === false);
+// console.log(parseFloat("fasdfa"));
+// Left off on exercise 10
